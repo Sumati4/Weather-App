709583296e75e6102f2949e83c33bfc24f0e7c0c
sumati4 <sumati3976@gmail.com>
2024-07-20 20:52:48 +0530

    initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,8 @@
+
+// Load environment variables from .env file
+
+
+module.exports = {
+  key:  process.env.REACT_APP_OPENWEATHERMAP_API_KEY,
+  base: process.env.REACT_APP_OPENWEATHERMAP_BASE_URL,
+};
